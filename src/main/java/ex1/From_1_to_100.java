9385aeff4f32e29aba3ba5cc2b9e5457dd44d49f
VladBogda <vladbogdantsev@gmail.com>
2023-06-01 17:50:44 +0300

    ex1

---
+++ src/main/java/ex1/From_1_to_100.java
@@ -0,0 +1,10 @@
+package ex1;
+
+public class From_1_to_100 {
+    public static void main(String[] args) {
+
+        for (int i=0; i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
