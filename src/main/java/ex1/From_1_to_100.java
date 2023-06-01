3ea0c5f22ddbf3f9b853a7b413d8817d80864a0d
VladBogda <vladbogdantsev@gmail.com>
2023-06-01 18:21:59 +0300

    ex1

--- src/main/java/ex1/From_1_to_100.java
+++ src/main/java/ex1/From_1_to_100.java
@@ -3,8 +3,13 @@ package ex1;
 public class From_1_to_100 {
     public static void main(String[] args) {
 
-        for (int i=0; i<=100;i++){
-            System.out.println(i);
+        for (int i=1; i<=50;i++){
+            if(i<=25){
+
+
+                System.out.println(i);
+            }
+
         }
     }
 }
