c50f50d8bd20335c66a2ad0f35a4eddd30a9f3b7
VladBogda <vladbogdantsev@gmail.com>
2023-06-01 19:07:56 +0300

    test

---
+++ src/main/java/ex1/From_1_to_100.java
@@ -0,0 +1,24 @@
+package ex1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class From_1_to_100 {
+    public static void main(String[] args) {
+
+        int [] array = {1,2,3,4,5,4,78,11,9};
+        for (Integer item: array){
+            System.out.println(item);
+        }
+
+
+        /*for (int i=1; i<=50;i++){
+            if(i<=25){
+                System.out.println(i);
+            }
+        }*/
+    }
+    static void sort(int [] arr){
+
+    }
+}
