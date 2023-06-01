e4c00f4f736f8490892ce2c49f8132a6f1696cc7
VladBogda <vladbogdantsev@gmail.com>
2023-06-01 19:24:58 +0300

    test commit

--- src/main/java/ex1/From_1_to_100.java
+++ src/main/java/ex1/From_1_to_100.java
@@ -1,38 +1,23 @@
 package ex1;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class From_1_to_100 {
     public static void main(String[] args) {
 
-        int [] array = {1,2,3,4,5,4,78,11,9};
-        for (Integer item: array){
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("array:");
+        for (Integer item : array) {
             System.out.println(item);
         }
 
-
-        /*for (int i=1; i<=50;i++){
-            if(i<=25){
-                System.out.println(i);
+        System.out.println("----------");
+        for (int i = 1; i <= 10; i++) {
+            if (i <= 5) {
+                System.out.println(i + ") i=" + i);
             }
-        }*/
-    }
-    static void sort(int [] arr){
-
-=======
-public class From_1_to_100 {
-    public static void main(String[] args) {
-
-        for (int i=1; i<=50;i++){
-            if(i<=25){
-
-
-                System.out.println(i);
-            }
-
         }
->>>>>>> 30df159aebab8e25adb241884cef9277e5d0321f
     }
 }
+
+
+
+
